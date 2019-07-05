ea929fb0f48b9c4b2201178c4d70f853baa80e6b
svetultus <svetultus@gmail.com>
2019-07-05 18:51:33 +0300

    usersList, photo

--- index.js
+++ index.js
@@ -2,16 +2,32 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {serveClient: true});
+const fs = require('fs');
+const bodyParser = require('body-parser')
+//const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 let users = [];
 let connections = [];
+let chatHistory = [];
 
+//app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(express.static(__dirname));
 //указываем корневую деректорию
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
+app.post('/uploadAvatar', function(req, res) {
+  // console.log(req);
+  // let buf = req.response;
+
+  // fs.writeFile('image.png', function(err, buf){
+  //   it's possible to embed binary data
+  //   within arbitrarily-complex objects
+  //   socket.emit('image', { image: true, buffer: buf });
+  // });
+});
+
 
 io.on('connection', function (socket) {
   
@@ -22,9 +38,11 @@ io.on('connection', function (socket) {
   socket.on('msgToServer', function (msg) {
     const obj = {
       date: new Date(),
-      content: msg,
-      username: socket.id
+      content: msg.message,
+      user: msg.user
     }
+    chatHistory.push(obj);
+    //saveHistoryInStorage ();
     socket.emit('messageAddedToServer', obj)
     socket.to('all').emit('messageAddedToServer', obj)
   });
@@ -35,17 +53,31 @@ io.on('connection', function (socket) {
 
   socket.on('authSend', (obj)=> {
     socket.emit('userAuthorized', obj);
-    socket.emit('memberListChanged', obj);
-    socket.to('all').emit('memberListChanged', obj);
+    // if (getHistoryFromStorage) {
+    //   socket.emit('receiveHistory', chatHistory);
+    // }
+    if ( !users.some((elem) => {
+      return elem.nickName === obj.nickName;
+    })) {
+      users.push(obj);   
+    };
+    socket.to('all').emit('memberListChanged', users);
+    socket.emit('memberListChanged', users);
+    socket.emit('historySend', chatHistory);
   });
 
-  socket.on('userAvatarToServer', ()=>{
+  socket.on('userAvatarToServer', (user)=>{
+    
     console.log('avatarToServer');
-    socket.emit('updateUserAvatar');
-    socket.to('all').emit('updateUserAvatar');
+    //console.log(file);
+    users[user['nickName']] = user;
+   
+    socket.emit('updateUserAvatar', user);
+    socket.emit('memberListChanged', users);
+    socket.to('all').emit('memberListChanged', users);
   })
-
 });
+
 // io.on('send mess', (data) => {
 //   io.socket.emit('new mess', messageData);
 // });
--- js/script.js
+++ js/script.js
@@ -19,38 +19,46 @@ let name = document.querySelector('#name'),
     container = document.querySelector('.container'),
     nameOfuser = document.querySelector('.users__block_name'),
     members = document.querySelector('.users__block_list'),
-    usersList = document.querySelector('#usersList'),
     messagesPhoto = document.querySelector('#messagesPhoto'),
     templateOfMessage = document.querySelector('#messageList').textContent,
     templateOfUsers = document.querySelector('#listOfUsers').textContent,
     renderUsers = Handlebars.compile(templateOfUsers),
     renderMessages = Handlebars.compile(templateOfMessage),
-    users = [],
-    messagesListOnPage = [],
+    //messagesListOnPage = [],
     socket = io.connect('http://localhost:3000/'),
-    max = 512000;
+    maxFileSize = 512000,
+    currentUser = {
+      nickName: '',
+      name: ''
+    },
+    chatHistory = [];
+    let storage;
 
   socket.on('connected', function (msg) {
-    //socket.emit('receiveHistory');
+    //socket.emit('receivechatHistory');
+    if (storageAvailable('localStorage')) {
+      storage = localStorage;
+    };
   });
-
   socket.on('messageAddedToServer', addMessage);
   socket.on('userAuthorized', userAuthorized);
   socket.on('memberListChanged', updateMemberList);
-  socket.on('updateUserAvatar', () => {console.log('updateUserAvatar')});
+  socket.on('updateUserAvatar', (user) => {
+    console.log('updateUserAvatar');
+    console.log(user);
+
+  });
+  socket.on('historySend', updateHistoryFormServer);
 
   sendBtn.addEventListener('click', (event) => {
     event.preventDefault();
 
-    let messageData = messageText.value.trim();
-    let messageInfo = {
-      // name: name.value.trim() + ':',
-      message: messageData
-    };
+    let messageData = {
+      user: currentUser,
+      message: messageText.value.trim()
+    }
 
-    
     if(messageData.length == 0){
-      
       messageText.classList.add('message__form_input-error');
     }
     socket.emit('msgToServer', messageData);
@@ -63,7 +71,9 @@ let name = document.querySelector('#name'),
     event.preventDefault();
 
     if (name.value && nickname.value) {
-      socket.emit('authSend', {'name': name.value, 'nickname': nickname.value});
+      currentUser.nickName = nickname.value;
+      currentUser.name = name.value;
+      socket.emit('authSend', currentUser);
     }
   })
 
@@ -96,7 +106,11 @@ let name = document.querySelector('#name'),
 
   fileloadForm.addEventListener('submit', (event)=>{
     event.preventDefault();
-    socket.emit('userAvatarToServer');
+
+    let bigImage = event.target.querySelector('.image_input');
+    // socket.emit('userAvatarToServer', inputFile.files[0]);
+    currentUser.avatar = bigImage.src;
+    socket.emit('userAvatarToServer', currentUser);
 
   });
 
@@ -115,8 +129,10 @@ let name = document.querySelector('#name'),
 
   function addMessage(message){
     console.log('messageAddedToServer');
-    messagesListOnPage.push(message);
-    let messagesList = renderMessages(messagesListOnPage);
+    chatHistory.push(message);
+    //saveChatHistoryInStorage();
+    //messagesListOnPage.push(message);
+    let messagesList = renderMessages(chatHistory);
           messages.innerHTML = messagesList;
           messageContainer.scrollTop = messageContainer.scrollHeight;
   }
@@ -124,13 +140,34 @@ let name = document.querySelector('#name'),
   function userAuthorized (obj) {
     nameOfuser.innerHTML = obj.name;
     toggleHide(authPopup);
+    // if (getChatHistoryFromStorage()) {
+    //   let messagesList = renderMessages(chatHistory);
+    //       messages.innerHTML = messagesList;
+    //       messageContainer.scrollTop = messageContainer.scrollHeight;
+    // };
   }
 
-  function updateMemberList (obj) {
-    const newMember = document.createElement('li');
+  function updateMemberList (users) {
+    let usersList = renderUsers(users);
+    members.innerHTML = usersList;
 
-    newMember.innerHTML = obj.name;
-    members.appendChild(newMember);
+      // let docFrag = document.createDocumentFragment();
+
+      // users.forEach((elem)=> {
+      //   const newMember = document.createElement('li');
+      //   const img = document.createElement('img');
+
+      //   newMember.dataset.nickname = elem.nickName;
+      //   newMember.innerHTML = elem.name;
+
+      //   img.src = elem.avatar;
+      //   img.alt = elem.name;
+      //   newMember.appendChild(img);
+
+      //   docFrag.appendChild(newMember);
+      // })
+      // members.innerHTML = "";
+      // members.appendChild(docFrag);
   }
 
   function toggleHide (element) {
@@ -147,7 +184,7 @@ let name = document.querySelector('#name'),
     const message = parent.querySelector('.inputFileMessage');
     let error = '';
 
-    if (file.size > max) {
+    if (file.size > maxFileSize) {
       error += 'Слишком большой файл. Допускаются файлы размером меньше 512 кБ. <br />';
     };
     if (file.type !== "image/jpeg") {
@@ -169,6 +206,37 @@ let name = document.querySelector('#name'),
     }
   }
 
+  function storageAvailable(type) {
+    try {
+      var storage = window[type],
+        x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    }
+    catch(e) {
+      return false;
+    }
+  }
   
+  function saveChatHistoryInStorage () {
+      storage['chatMessages'] = JSON.stringify(chatHistory);
+  }
+  
+  function getChatHistoryFromStorage () {
+      let chatMessages = JSON.parse(storage.getItem('chatMessages'));
+  
+      if (chatMessages !== null) {
+          chatHistory = chatMessages;
+      };
+      return true;
+  }
+  
+  function updateHistoryFormServer (chatHistoryFromServer) {
+    chatHistory = chatHistoryFromServer;
 
+    let messagesList = renderMessages(chatHistory);
+    messages.innerHTML = messagesList;
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+  }
 });
